449ec55ff2530f4387af9883f34a77980259b3bd
mehdi nasery pak <160924833+MehdiNaseryPak@users.noreply.github.com>
2025-12-15 18:58:56 +0330

    start project and make table migration

---
+++ database/migrations/2025_12_15_143607_create_posts_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('slug')->unique();
+            $table->text('summary');
+            $table->text('body');
+            $table->string('image');
+            $table->tinyInteger('commentable')->default(0);
+            $table->tinyInteger('status')->default(0);
+            $table->timestamp('published_at');
+            $table->foreignId('author_id')->constrained('users');
+            $table->foreignId('category_id')->constrained('post_categories');
+            $table->timestamps();
+            $table->softDeletes(); 
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('posts');
+    }
+};
---
+++ database/migrations/2025_12_15_143817_create_category_attribute_values_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('category_attribute_values', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('product_id')->constrained('products')->onUpdate('cascade')->onDelete('cascade');
+            $table->foreignId('category_attribute_id')->constrained('category_attributes')->onUpdate('cascade')->onDelete('cascade');
+            $table->text('values');
+            $table->tinyInteger('type')->default(0);
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('category_attribute_values');
+    }
+};
